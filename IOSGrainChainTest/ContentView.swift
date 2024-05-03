8528fae0471c94f4f6d557b5c0859fa97320bbc4
rickherrera3001 <rihr.3001@gmail.com>
2024-05-02 19:39:20 -0600

    split screen into two map and tableview

--- IOSGrainChainTest/ContentView.swift
+++ IOSGrainChainTest/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     var body: some View {
       //  NavigationSplitView {
             
-            ViewControllerBridge()
+                ViewControllerBridge()
             
          /*   List {
                 ForEach(items) { item in
--- Moduls/MapViewController.swift
+++ Moduls/MapViewController.swift
@@ -10,7 +10,7 @@ import GoogleMaps
 import CoreLocation
 import SwiftUI
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     // MARK: - Variables de instancia
     private var mapView: GMSMapView!  // Mapa de Google
     private var locationManager = CLLocationManager()  // Administrador de ubicación
@@ -24,21 +24,47 @@ class MapViewController: UIViewController {
     var endDate : Date = .now // Fecha de fin de la grabación
     let screenSize =  UIScreen.main.bounds // Tamaño de la pantalla
     
+    var tableView = UITableView()
     
     
+    // MARK: - Métodos de instancia
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupTableView()
+        
         
         let screenHeigh = screenSize.height/2 // Calcula el alto de la pantalla dividido por 2
         let rect = CGRect(x: 0, y: 0, width: screenHeigh, height: screenSize.width) // Define un rectángulo con el ancho y alto de la pantalla, intercambiados
         
-        
-        
-        // Configurar el mapa
         // Configurar el mapa con una posición y zoom específicos
         let camera = GMSCameraPosition.camera(withLatitude: 19.639073, longitude: -99.088213, zoom: 12)
         mapView = GMSMapView(frame: rect, camera: camera)
         view.addSubview(mapView)
+        
+        // Configurar la tabla
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        tableView = UITableView(frame:  CGRect(x: 0, y: view.frame.height / 2, width: view.frame.width, height: view.frame.height / 2))
+        
+        // Configurar la tabla
+        func setupTableView() {
+            tableView = UITableView(frame: CGRect(x: 0, y: view.frame.height / 2, width: view.frame.width, height: view.frame.height / 2))
+            tableView.backgroundColor = .gray
+            tableView.dataSource = self
+            tableView.delegate = self
+            view.addSubview(tableView)
+            
+            
+            tableView.register(routeListCell.self, forCellReuseIdentifier: "Cell")
+            tableView.dataSource = self
+            tableView.delegate = self
+            view.addSubview(tableView)
+        }
+        
         
         // Configurar el botón de grabación
         recordButton.setTitle("Iniciar Grabación", for: .normal)
@@ -56,6 +82,31 @@ class MapViewController: UIViewController {
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation() // Inicializar la ubicación del usuario
+        
+    }
+    
+    // MARK: - UITableViewDataSource
+    // Este método devuelve el número de filas en la sección especificada de la tabla.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Retorna la cantidad de elementos en el arreglo 'routes', que probablemente representan el número de rutas guardadas.
+        return routes.count
+    }
+    
+    // Este método configura y devuelve una celda para mostrar en la tabla en una ubicación específica.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! routeListCell
+        
+        let route = routes[indexPath.row]
+        cell.textLabel?.text = route.name
+        
+        let distanceInKilometers = route.distance / 1000
+        cell.detailTextLabel?.text = String(format: "%2f km" , distanceInKilometers)
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
     
     // MARK: - Métodos de grabación
@@ -66,8 +117,8 @@ class MapViewController: UIViewController {
         
         if !isRecording {
             // Agregar marcador al detener la grabación
-            if let lastLoacation = currentRouteCordinates.last {
-                addMarker(at: lastLoacation)
+            if let lastLocation = currentRouteCordinates.last {
+                addMarker(at: lastLocation)
             }
             showSaveRouteAlert()
             let distanceTraveled = calculateDistanceTraveled()
@@ -77,6 +128,7 @@ class MapViewController: UIViewController {
         }
     }
     
+    // MARK: - Métodos auxiliares
     // Función para calcular la distancia recorrida
     func calculateDistanceTraveled() -> Double {
         var distance: Double = 0.0
@@ -87,12 +139,11 @@ class MapViewController: UIViewController {
         for i in 1..<currentRouteCordinates.count {
             let startPoint = currentRouteCordinates[i-1]
             let endPoint = currentRouteCordinates[i]
-            let distanceBetwwenTwoPoints = startPoint.distance(from: endPoint)
-            distance += distanceBetwwenTwoPoints
+            let distanceBetweenTwoPoints = startPoint.distance(from: endPoint)
+            distance += distanceBetweenTwoPoints
         }
         return distance
     }
-    
     
     // Mostrar alerta para guardar la ruta
     func showSaveRouteAlert() {
@@ -124,19 +175,19 @@ class MapViewController: UIViewController {
         routes.append(route)
         clearRoute()
         print(routes)
+        tableView.reloadData()
     }
     
-    
-    // MARK: - Métodos auxiliares
     // Actualizar la polilínea con las nuevas ubicaciones
-    func updatePolyline(with location: [CLLocation]) {
-        for cordinates in location {
-            
-            let polyline = GMSPolyline(path: path)
-            polyline.strokeColor = .systemRed
-            polyline.strokeWidth = 3.0
-            polyline.map = mapView
+    func updatePolyline(with locations: [CLLocation]) {
+        for coordinate in locations {
+            path.add(coordinate.coordinate)
         }
+        
+        let polyline = GMSPolyline(path: path)
+        polyline.strokeColor = .systemRed
+        polyline.strokeWidth = 3.0
+        polyline.map = mapView
     }
     
     // Limpiar la ruta
@@ -149,7 +200,7 @@ class MapViewController: UIViewController {
     func addMarker(at coordinate: CLLocation) {
         let marker = GMSMarker()
         marker.position = coordinate.coordinate
-        marker.title = title
+        marker.title = coordinate.description // Set a title to marker
         marker.map = mapView
     }
 }
@@ -165,7 +216,7 @@ struct ViewControllerBridge: UIViewControllerRepresentable {
     }
 }
 
-// Extensión para manejar eventos de CLLocationManagerDelegate
+// Implementación de CLLocationManagerDelegate
 extension MapViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
@@ -178,8 +229,8 @@ extension MapViewController: CLLocationManagerDelegate {
                 addMarker(at: location)
             }
             
-            path.add(location.coordinate)
             currentRouteCordinates.append(location)
+            updatePolyline(with: currentRouteCordinates)
             
             // Centrar el mapa en la última ubicación
             let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: 15)
@@ -190,8 +241,25 @@ extension MapViewController: CLLocationManagerDelegate {
     // Añadir ubicación a la ruta
     func addLocationToRoute(_ coordinate: CLLocation) {
         currentRouteCordinates.append(coordinate)
-        updatePolyline(with: currentRouteCordinates)
     }
 }
+
+// Definición de la clase UITableViewCell para las celdas de la tabla
+class routeListCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier:  reuseIdentifier)
+        
+        // Configurar las propiedades de la celda
+        textLabel?.numberOfLines = 0 // Permite múltiples líneas en el título
+        detailTextLabel?.numberOfLines = 0 // Permite múltiples líneas en el detalle
+    }
+    // Este método es requerido para conformarse al protocolo NSCoding, pero en este caso se implementa como una fatal error.
+    required init?(coder: NSCoder) {
+        // Lanza un error fatal indicando que la inicialización desde un codificador no ha sido implementada.
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
 
 
